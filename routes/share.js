4a15df2e307b66bd266767d0170c2c466353cc46
RoberMac <abc269513055@gmail.com>
2016-02-05 23:00:41 +0800

    hotfix-0.10.4

--- routes/share.js
+++ routes/share.js
@@ -109,7 +109,7 @@ router.get('/:provider/:id', (req, res, next) => {
                                 data: found.data,
                                 viewCount: found.viewCount
                             },
-                            isBlocked: !!req.acceptsLanguages('zh-cn', 'pa-pk', 'ko-kp', 'fa-ir')
+                            isBlocked: !!req.acceptsLanguages('zh', 'pa-pk', 'ko-kp', 'fa-ir')
                         })
                     }
                 })
