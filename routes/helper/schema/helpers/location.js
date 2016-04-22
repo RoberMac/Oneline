fcd1a26571edcc8e13a26a63e99b9cd32f35357a
RoberMac <abc269513055@gmail.com>
2016-04-22 22:06:52 +0800

    v0.13.7-rc.4
    - fix: Joi Schema: location.place_id is optional

--- routes/helper/schema/helpers/location.js
+++ routes/helper/schema/helpers/location.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 module.exports = Joi.object({
-    name: Joi.string().required(),
-    id  : Joi.alternatives().try(Joi.number(), Joi.string()).optional(),
-    // place_id // [w]
+    name    : Joi.string().required(),
+    id      : Joi.alternatives().try(Joi.number(), Joi.string()).optional(),
+    place_id: Joi.string().optional(),
 });
